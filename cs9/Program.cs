1f2aec945d2163a2145ad182148aeacfdf8c31af
Alex Pokislyuk <keyprog@gmail.com>
2021-04-10 22:32:18 -0400

    Added args and record declaration

--- cs9/Program.cs
+++ cs9/Program.cs
@@ -1,6 +1,13 @@
 using System;
 using System.Net.Http;
 
-string response = await new HttpClient().GetStringAsync("https://raw.githubusercontent.com/keyprog/dotnet-lab/master/README.md");
+const string DEFAULT_URL = "https://raw.githubusercontent.com/keyprog/dotnet-lab/master/README.md";
+
+string url = args?.Length > 0 ? args[0] : DEFAULT_URL;
+string response = await new HttpClient().GetStringAsync(url);
 
 Console.WriteLine(response);
+
+Console.WriteLine(new MyData("Alex","123"));
+
+record MyData (string Name, string Code);
